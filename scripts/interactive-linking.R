a70d6da45b8518e5c1e006de7609295ce4775515
Sherry Zhang <huizezhangsh@gmail.com>
2021-12-16 17:12:34 +1100

    update on interactive graphics

--- scripts/interactive-linking.R
+++ scripts/interactive-linking.R
@@ -1,16 +1,20 @@
 library(cubble)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
+library(crosstalk)
+library(plotly)
 
 clean <-  weatherdata::climate_full %>%
+  filter(!id %in% c("ASN00067033", "ASN00072091")) %>% 
   stretch() %>%
-  mutate(month = month(date),
+  mutate(month = lubridate::month(date),
          diff = tmax - tmin) %>% 
   group_by(month) %>% 
   summarise(tmax = mean(tmax, na.rm = TRUE),
             diff = mean(diff, na.rm = TRUE)) %>% 
   ungroup(month) %>% 
   tamp() %>%
+  mutate(a = nrow(ts)) %>% 
+  filter(a == 12) %>% 
   mutate(diff_winter = ts %>% filter(month %in% c(6,7,8)) %>% pull(diff) %>% mean(na.rm = TRUE),
          diff_summer = ts %>% filter(month %in% c(12, 1,2)) %>% pull(diff) %>% mean(na.rm = TRUE),
          diff_year = diff_winter - diff_summer) %>% 
@@ -21,11 +25,11 @@ clean <-  weatherdata::climate_full %>%
 long <- clean %>% 
   SharedData$new(~id, group = "cubble")
 
-nested <- clean %>% tamp() %>% 
+nested <- clean %>% tamp() %>% as_tibble() %>% 
   SharedData$new(~id, group = "cubble")
 
 
-p1 <- nested %>% 
+p1 <- nested %>%  
   ggplot(aes(x = diff_summer, y = diff_winter, color = diff_year, label = name)) + 
   geom_point() + 
   geom_abline(slope = 1, intercept = 0, linetype = "dashed") + 
@@ -38,7 +42,7 @@ p1 <- nested %>%
   ylab("temperature difference in winter") 
 
 state_map <- rmapshaper::ms_simplify(ozmaps::abs_ste, keep = 2e-3)
-p2 <- nested %>%
+p2 <- nested %>% 
   ggplot() +
   geom_sf(data = state_map, aes(geometry = geometry), fill = "transparent") +
   geom_point(aes(x = long, y = lat, label = name, color = diff_year)) +
@@ -50,13 +54,17 @@ p2 <- nested %>%
   theme(panel.grid.major = element_blank(),
         legend.position = "bottom")
 
+
+
 out <- bscols(
-  ggplotly(p1, width = 800, height = 800, tooltip = "label") %>%
-    highlight(on = "plotly_selected", off = "plotly_deselect", color = "red"),
-  ggplotly(p2, width = 900, height = 800, tooltip = "label") %>%
-    highlight(on = "plotly_selected", off = "plotly_deselect", color = "red")
-  
+  ggplotly(p1, width = 700, height = 700, tooltip = "label") %>% 
+    highlight(on = "plotly_selected", off = "plotly_deselect",
+              selected = attrs_selected(color = "grey10")),
+  ggplotly(p2, width = 900, height = 700, tooltip = "label") %>% 
+    highlight(on = "plotly_selected", off = "plotly_deselect",
+              selected = attrs_selected(color = "grey10")),
+  widths = c(6, 6)
 )
 
 htmltools::save_html(out, file = "figures/linking.html")
-webshot2::webshot("figures/linking.html", "figures/linking.png", vwidth = 1800, vheight = 2000)
+webshot2::webshot("figures/linking.html", "figures/linking.png", vwidth = 1900, vheight = 2000)
--- scripts/interactive-popup.R
+++ scripts/interactive-popup.R
@@ -1,53 +1,55 @@
 library(cubble)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 library(leaflet)
 library(leafpop)
 
-tas <- weatherdata::climate_full %>%
-  filter(stringr::str_sub(id, 7, 8) > 90) %>% 
+clean <-  weatherdata::climate_full %>%
+  filter(!id %in% c("ASN00067033", "ASN00072091")) %>% 
   stretch() %>%
-  mutate(month = month(date),
+  mutate(month = lubridate::month(date),
          diff = tmax - tmin) %>% 
-  filter(month %in% c(12, 1,2, 6,7,8)) %>% 
-  mutate(dummy_date = as.Date(glue::glue("2021-{month}-01")),
-         season = case_when(month %in% c(12, 1, 2) ~ "winter",
-                            month %in% c(6,7,8) ~ "summer"))
+  group_by(month) %>% 
+  summarise(tmax = mean(tmax, na.rm = TRUE),
+            diff = mean(diff, na.rm = TRUE)) %>% 
+  ungroup(month) %>% 
+  tamp() %>%
+  mutate(a = nrow(ts)) %>% 
+  filter(a == 12) %>% 
+  mutate(diff_winter = ts %>% filter(month %in% c(6,7,8)) %>% pull(diff) %>% mean(na.rm = TRUE),
+         diff_summer = ts %>% filter(month %in% c(12, 1,2)) %>% pull(diff) %>% mean(na.rm = TRUE),
+         diff_year = diff_winter - diff_summer) %>% 
+  stretch() %>% 
+  mutate(dummy_date = as.Date(glue::glue("2021-{month}-01"))) %>%
+  migrate(name) 
 
 i <- 1
-df_id <- tas$id %>% unique()
+df_id <- clean$id %>% unique()
 p <- vector("list", length(df_id))
-name <- unique(tas$name)
 for (i in 1:length(df_id)) {
-  dt <- tas %>% 
+  dt <- clean %>% 
     filter(id == df_id[i]) 
   
   p[[i]] <- dt %>% 
-    ggplot() + 
-    geom_histogram(aes(x = diff, group = season), binwidth = 1) + 
-    geom_vline(aes(xintercept = mean(diff, na.rm = TRUE)), 
-               color = "red", data = ~ filter(.x, season == "summer")) + 
-    geom_vline(aes(xintercept = mean(diff, na.rm = TRUE)), 
-               color = "red", data = ~ filter(.x, season == "winter")) + 
-    facet_wrap(vars(season), ncol = 1) + 
+    ggplot(aes(x = dummy_date, ymin = tmax - diff, ymax = tmax, group = id)) + 
+    geom_ribbon(color = "grey50", fill = "grey50", alpha = 0.5) + 
     theme_bw() + 
-    xlab("temperature difference")
+    scale_x_date(date_labels = "%b", date_breaks = "1 month") + 
+    labs(x = "Month", y = "Temperature") + 
+    ylim(-10, 50)
   
   i <- i + 1
   
 }
 
-tas_nested <- tamp(tas) %>%
-  mutate(diff_winter = ts %>% filter(month %in% c(6,7,8)) %>% pull(diff) %>% mean(na.rm = TRUE),
-         diff_summer = ts %>% filter(month %in% c(12, 1,2)) %>% pull(diff) %>% mean(na.rm = TRUE),
-         diff_year = diff_winter - diff_summer)
-
-domain <- range(tas_nested$diff_year)
-pal <- colorNumeric(colorspace::diverging_hcl(palette = "Purple-Green", n = 100),
+nested <- tamp(clean) 
+domain <- nested$diff_year
+pal <- colorNumeric(colorspace::diverging_hcl(palette = "Purple-Green", 
+                                              n = 11, l2 = 80),
                     domain = domain)
 
-leaflet() %>% 
+out <- leaflet(nested) %>% 
   addTiles() %>% 
-  addCircleMarkers(tas_nested, group = "a") %>% 
+  addCircleMarkers(group = "a", color = ~pal(domain), radius = 0.1) %>% 
   addPopupGraphs(graph = p, group = "a", width = 300, height =200)
 
+htmlwidgets::saveWidget(out, file = "figures/popup.html")
