f31ef39d6db89bb31e2bdeb2a2174bdd7efeecf6
Sherry Zhang <huizezhangsh@gmail.com>
2021-12-20 13:02:57 +1100

    basic manip sexctionon almost done:

--- scripts/basic-manip.R
+++ scripts/basic-manip.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+library(GGally)
 tmax <- weatherdata::historical_tmax %>%
   # filter out VIC and NSW stations
   filter(between(stringr::str_sub(id, 7, 8), 46, 90)) %>% 
@@ -21,17 +23,55 @@ tmax <- weatherdata::historical_tmax %>%
   migrate(latitude, longitude)
 
 gly <- glyphs(tmax, "longitude", "month", "latitude", "tmax",
-              height = 0.6, width = 1)
+              height = 0.5, width = 1)
 
 nsw_vic <- ozmaps::abs_ste %>% filter(NAME %in% c("Victoria", "New South Wales"))
 
-ggplot() + 
-  geom_sf(data = nsw_vic, fill = "transparent") + 
+p1 <- ggplot() + 
+  geom_sf(data = nsw_vic, fill = "transparent", color = "grey", linetype = "dotted") + 
   geom_path(data = gly, 
             aes(gx, gy, group = interaction(id, group), color = group)) +
   scale_color_brewer(palette = "Dark2") + 
   theme_bw() + 
-  coord_sf(xlim = c(140, 155)) + 
-  theme(legend.position = "bottom")
+  coord_sf(xlim = c(141, 154)) + 
+  theme(legend.position = "bottom") +
+  labs(x = "Longitude", y = "Latitude")
+
+#################
+# inset
+tmax %>% filter(id == "ASN00048027") %>%
+  ggplot(aes(x = month,
+             y = tmax,
+             color = group)) +
+  geom_line(size = 1.5) +
+  scale_color_brewer(palette = "Dark2", guide = "none") +
+  scale_x_continuous(breaks = seq(1, 12, 1)) +
+  labs(x = "Month", y  = "Temperature", title = "ASN00048027: Cobar") +
+  theme_bw() +
+  theme(
+    aspect.ratio = 0.3,
+    axis.text = element_text(size = 20),
+    title =  element_text(size = 20)
+  )
+
+#ggsave(filename = here::here("figures/basic-manip-inset.png"), width = 7)
+
+box_df <- tmax %>% tamp() %>% filter(id == "ASN00048027")
+single <-
+  tibble::tibble(img = here::here("figures/basic-manip-inset.png"))
+p1 +
+  geom_rect(
+    data = box_df,
+    aes(
+      xmin = longitude - 0.6,
+      xmax = longitude + 0.6,
+      ymin = latitude - 0.12,
+      ymax = latitude + 0.35
+    ),
+    fill = "transparent",
+    color = "black"
+  ) +
+  ggimg::geom_point_img(data = single,
+                        aes(x = 143, y = -29, img = img), size = 6)
 
 ggsave(filename = "figures/basic-manip.png", width = 10, height = 10)
