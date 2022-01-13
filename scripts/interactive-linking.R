1d0f94f7cb3337edb94e2808286f67d4c13bbd4c
Sherry Zhang <huizezhangsh@gmail.com>
2022-01-13 23:09:32 +1100

    interactive graphic crosstalk done

--- scripts/interactive-linking.R
+++ scripts/interactive-linking.R
@@ -3,65 +3,64 @@ library(tidyverse)
 library(crosstalk)
 library(plotly)
 
-clean <-  weatherdata::climate_full %>%
-  filter(!id %in% c("ASN00067033", "ASN00072091", "ASN00059040", "ASN00097053")) %>%
-  stretch() %>%
-  mutate(month = lubridate::month(date),
-         diff = tmax - tmin,
-         avg = (tmax + tmin)/2) %>%
+clean <- weatherdata::climate_full %>%
+  filter(!id %in% c("ASN00067033", "ASN00072091", "ASN00059040", 
+                    "ASN00097053", "ASN00040856", "ASN00015548")) %>%
+  stretch() %>% 
+  mutate(month = lubridate::month(date)) %>%
   group_by(month) %>%
   summarise(tmax = mean(tmax, na.rm = TRUE),
             tmin = mean(tmin, na.rm = TRUE),
-            diff = mean(diff, na.rm = TRUE),
-            avg = mean(avg, na.rm =TRUE)) %>%
+            diff = mean(tmax - tmin, na.rm = TRUE)) %>% 
   ungroup(month) %>%
   mutate(dummy_date = as.Date(glue::glue("2021-{month}-01"))) %>% 
-  tamp() %>%
-  mutate(
-    diff_winter = ts %>% filter(month %in% c(6, 7, 8)) %>% pull(diff) %>% mean(na.rm = TRUE),
-    diff_summer = ts %>% filter(month %in% c(12, 1, 2)) %>% pull(diff) %>% mean(na.rm = TRUE),
-    diff_year = diff_winter - diff_summer,
-    annual = ts %>% filter(month %in% c(7, 1)) %>% pull(avg) %>% diff() %>% abs(),
-    diurnal = ts %>% pull(diff) %>% mean(na.rm = TRUE)
-  )
+  tamp() %>% 
+  mutate(temp_diff_var = ts %>% pull(diff) %>% var(na.rm = TRUE))
+  
 
 nested <- clean %>% 
   SharedData$new(~id, group = "cubble")
 
-p1 <- nested %>%  
-  ggplot(aes(x = diff_summer, y = diff_winter, color = diff_year, label = name)) + 
-  geom_point() + 
-  geom_abline(slope = 1, intercept = 0, linetype = "dashed") + 
-  colorspace::scale_color_continuous_diverging("Purple-Green", mid = 0, l2 = 80) + 
-  xlim(0, 25) + 
-  ylim(0, 25) +
-  theme_bw() + 
-  theme(legend.position = "none") + 
-  xlab("temperature difference in summer") + 
-  ylab("temperature difference in winter") 
+long <- clean %>% stretch() %>% migrate(temp_diff_var) %>% arrange(temp_diff_var) %>% 
+  SharedData$new(~id, group = "cubble")
 
 state_map <- rmapshaper::ms_simplify(ozmaps::abs_ste, keep = 2e-3)
-p2 <- nested %>% 
+p1 <- nested %>% 
   ggplot() +
-  geom_sf(data = state_map, aes(geometry = geometry), fill = "transparent") +
-  geom_point(aes(x = long, y = lat, label = name, color = diff_year)) +
-  colorspace::scale_color_continuous_diverging(
-    "Purple-Green", 
-    mid = 0, l2 = 80,
-    name = "winter-summer difference") + 
+  geom_sf(data = state_map, aes(geometry = geometry), fill = "transparent", color = "grey50") +
+  geom_point(aes(x = long, y = lat,color = temp_diff_var, label = name)) +
+  colorspace::scale_color_continuous_sequential(
+    "Rocket",  n_interp = 7, cmax = 90, rev = TRUE, c2 = 40, l2= 85, c1 = 20, l1 = 30,
+    name = "Var. temp. diff.") +
+  labs(x = "Longitude", y = "Latitude") + 
+  coord_sf() + 
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(),
+        legend.position = "none")
+
+p2 <- long %>% 
+  ggplot(aes(x = dummy_date, group = id, fill = temp_diff_var, color = temp_diff_var)) +
+  geom_ribbon(aes(ymin = tmin, ymax = tmax), size = 0.1) + 
+  geom_point(aes(y = tmax), size = 0.1) + 
+  geom_point(aes(y = tmin), size = 0.1) + 
+  colorspace::scale_fill_continuous_sequential(
+    "Rocket",  n_interp = 7, cmax = 90, rev = TRUE, c2 = 40, l2= 85, c1 = 20, l1 = 30,
+    name = "Var. temp. diff.") +
+  colorspace::scale_colour_continuous_sequential(
+    "Rocket",  n_interp = 7, cmax = 90, rev = TRUE, c2 = 40, l2= 85, c1 = 20, l1 = 30,
+    name = "Var. temp. diff.") +
+  scale_x_date(date_labels = "%b", date_breaks = "1 month") + 
+  labs(x = "Month", y = "Temperature") + 
   theme_bw() + 
   theme(panel.grid.major = element_blank(),
         legend.position = "bottom")
 
 out <- bscols(
-  ggplotly(p1, width = 700, height = 700, tooltip = "label") %>% 
-    highlight(on = "plotly_selected", off = "plotly_deselect",
-              selected = attrs_selected(color = "grey10")),
-  ggplotly(p2, width = 900, height = 700, tooltip = "label") %>% 
-    highlight(on = "plotly_selected", off = "plotly_deselect",
-              selected = attrs_selected(color = "grey10")),
-  widths = c(6, 6)
+  ggplotly(p1, width = 900, height = 700, tooltip = "label") %>%
+    highlight(on = "plotly_selected", off = "plotly_deselect", opacityDim = 0.1),
+  ggplotly(p2, width = 1200, height = 700, tooltip = "label") %>% 
+    highlight(on = "plotly_selected", off = "plotly_deselect", opacityDim = 0.01),
+  widths = c(5, 5)
 )
 
 htmltools::save_html(out, file = "figures/linking.html")
-webshot2::webshot("figures/linking.html", "figures/linking.png", vwidth = 1900, vheight = 2000)
