45ca46cead064fcedd5ca90fde08083d3f6ac71e
Sherry Zhang <huizezhangsh@gmail.com>
2022-01-15 16:45:34 +1100

    interactive graphic sectionsection done

--- scripts/interactive-popup.R
+++ scripts/interactive-popup.R
@@ -4,31 +4,26 @@ library(leaflet)
 library(leafpop)
 
 clean <-  weatherdata::climate_full %>%
-  filter(!id %in% c("ASN00067033", "ASN00072091")) %>% 
+  filter(!id %in% c("ASN00067033", "ASN00072091", "ASN00059040", 
+                    "ASN00097053", "ASN00040856", "ASN00015548")) %>%
   stretch() %>%
-  mutate(month = lubridate::month(date),
-         diff = tmax - tmin) %>% 
+  mutate(month = lubridate::month(date)) %>% 
   group_by(month) %>% 
   summarise(tmax = mean(tmax, na.rm = TRUE),
-            diff = mean(diff, na.rm = TRUE)) %>% 
+            tmin = mean(tmin, na.rm = TRUE),
+            diff = mean(tmax - tmin, na.rm = TRUE)) %>% 
   ungroup(month) %>% 
-  mutate(dummy_date = as.Date(glue::glue("2021-{month}-01"))) %>% 
   tamp() %>%
-  mutate(a = nrow(ts)) %>% 
-  filter(a == 12) %>% 
-  mutate(diff_winter = ts %>% filter(month %in% c(6,7,8)) %>% pull(diff) %>% mean(na.rm = TRUE),
-         diff_summer = ts %>% filter(month %in% c(12, 1,2)) %>% pull(diff) %>% mean(na.rm = TRUE),
-         diff_year = diff_winter - diff_summer) %>% 
+  mutate(temp_diff_var = ts %>% pull(diff) %>% var(na.rm = TRUE)) %>% 
   stretch() %>% 
-  mutate(dummy_date = as.Date(glue::glue("2021-{month}-01"))) %>%
-  migrate(name) 
-
-i <- 1
+  mutate(dummy_date = as.Date(glue::glue("2021-{month}-01"))) %>% 
+  migrate(name)
+  
 df_id <- clean$id %>% unique()
 p <- purrr::map(1:length(df_id), function(i){
   dt <- clean %>% filter(id == df_id[i])
   dt %>% 
-    ggplot(aes(x = dummy_date, ymin = tmax - diff, ymax = tmax, group = id)) + 
+    ggplot(aes(x = dummy_date, ymin = tmin, ymax = tmax, group = id)) + 
     geom_ribbon(color = "grey50", fill = "grey50", alpha = 0.5) + 
     theme_bw() + 
     scale_x_date(date_labels = "%b", date_breaks = "1 month") + 
@@ -37,33 +32,17 @@ p <- purrr::map(1:length(df_id), function(i){
     ggtitle(paste0(stringr::str_to_title(unique(dt$name))))
 })
 
-
-for (i in 1:length(df_id)) {
-  dt <- clean %>% 
-    filter(id == df_id[i]) 
-  
-  p[[i]] <- dt %>% 
-    ggplot(aes(x = dummy_date, ymin = tmax - diff, ymax = tmax, group = id)) + 
-    geom_ribbon(color = "grey50", fill = "grey50", alpha = 0.5) + 
-    theme_bw() + 
-    scale_x_date(date_labels = "%b", date_breaks = "1 month") + 
-    labs(x = "Month", y = "Temperature") + 
-    ylim(-10, 50) + 
-    ggtitle(paste0(stringr::str_to_title(unique(dt$name))))
-  
-  i <- i + 1
-  
-}
-
 nested <- tamp(clean) 
-domain <- nested$diff_year
-pal <- colorNumeric(colorspace::sequential_hcl(palette = "Oslo", 
-                                              n = 11),
-                    domain = domain)
+domain <- nested$temp_diff_var
+pal <- colorNumeric(
+  colorspace::sequential_hcl(
+    "Rocket",  n = 7, cmax = 90, rev = TRUE, 
+    c2 = 40, l2= 85, c1 = 20, l1 = 30),
+  domain = domain)
 
 out <- leaflet(nested) %>% 
   addTiles() %>% 
-  addCircleMarkers(group = "a", color = ~pal(domain), radius = 0.1) %>% 
+  addCircleMarkers(color = ~pal(domain), radius = 0.1, fillOpacity = 1, opacity = 1) %>% 
   addPopupGraphs(graph = p, group = "a", width = 300, height =200)
 
 htmlwidgets::saveWidget(out, file = "figures/popup.html")
