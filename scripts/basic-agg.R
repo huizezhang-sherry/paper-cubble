10c235a9303dfb3e71940f779b414d27b63593d8
Sherry Zhang <huizezhangsh@gmail.com>
2022-01-19 16:29:58 +1100

    aggregation example WIP

--- scripts/basic-agg.R
+++ scripts/basic-agg.R
@@ -1,51 +1,56 @@
 library(tidyverse)
 library(cubble)
+library(patchwork)
 coords <- cbind(weatherdata::climate_full$long, weatherdata::climate_full$lat)
 dist_raw <- geosphere::distm(coords, coords)
 
+set.seed(123)
 station_nested <- weatherdata::climate_full %>% 
   strip_rowwise() %>% 
   mutate(cluster = kmeans(dist_raw,centers = 20, nstart = 500)$cluster)
+save(station_nested, file = here::here("data/station_nested.rda"))
 
 cluster_nested <- station_nested %>%
   switch_key(cluster) %>% 
   get_centroid()
 
-station_long <- cluster_nested %>% 
+station_long <- station_nested %>% 
   stretch(ts) %>% 
   mutate(wk = lubridate::week(date)) %>% 
-  group_by(wk, id) %>% 
+  group_by(wk) %>% 
   summarise(prcp = sum(prcp, na.rm = TRUE))
 
-cluster_long <- station_long %>% 
-  ungroup(id) %>% 
+cluster_long <- cluster_nested %>% 
+  stretch(ts) %>% 
+  mutate(wk = lubridate::week(date)) %>% 
+  group_by(wk) %>% 
   summarise(prcp = mean(prcp, na.rm = TRUE)) %>% 
   migrate(cent_long, cent_lat)
-
-gly <- GGally::glyphs(cluster_long,
-                      x_major = "cent_long", x_minor = "wk",
-                      y_major = "cent_lat", y_minor = "prcp",
-                      height = 2, width = 4
-)
 
 state_map <- rmapshaper::ms_simplify(ozmaps::abs_ste, keep = 2e-3)
 p1 <- plot_map(state_map) +
   geom_text(data = cluster_nested, 
             aes(x = cent_long, y = cent_lat, label = cluster)) +
-  geom_path(data = gly,aes(x = gx, y = gy, group = gid))
+  geom_glyph(data = cluster_long, 
+             aes(x_major = cent_long, x_minor = wk, y_major = cent_lat, y_minor = prcp), 
+             height = 2, width = 4)
 
+tas_latlong <- station_long %>% 
+  ungroup(wk, id) %>% 
+  filter(cluster == 7) %>% 
+  filter(prcp == max(prcp)) %>% 
+  tamp()
+
+tas_id <- tas_latlong %>% pull(id)
+tas_long <- station_long %>% ungroup(wk, id) %>% filter(id == tas_id)
 
 p2 <- station_long %>%
   ggplot(aes(x = wk, y = prcp, group = id)) +
   geom_line(alpha = .3) +
+  geom_line(data = tas_long, color = "red") + 
   facet_wrap(vars(cluster), scales = "free_y", ncol = 4) +
   theme_bw()
 
-tas_latlong <- station_long %>% 
-  ungroup(wk, id) %>% 
-  filter(cluster == 15) %>% 
-  filter(prcp == max(prcp)) %>% 
-  tamp()
 
 p3 <- plot_map(state_map) +
   geom_point(data = station_nested, aes(x = long, y = lat), size = 0.5) +
