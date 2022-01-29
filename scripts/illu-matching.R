39ab92a09a087c65924f1c3a0aeb04fd609e1d2d
Sherry Zhang <huizezhangsh@gmail.com>
2022-01-29 15:45:39 +1100

    more work on matching in the advanced feature section

--- scripts/illu-matching.R
+++ scripts/illu-matching.R
@@ -16,8 +16,9 @@ circle <- tibble(
   x = c(6, 14, 23, 7, 21, 27),
   y = c(10, 5, 7, 7, 4, 6),
   id = factor(c(rep("A", 3), rep("a", 3)), levels = c("A", "a")),
-  xend = x + 5
-)
+  xend = x + 5,
+  match = factor(c("yes", "no", rep("yes", 2), "no", "yes"), levels = c("yes", "no"))
+) 
 
 errorbar <- bind_rows(
   circle %>% filter(id == "A"),
@@ -25,24 +26,19 @@ errorbar <- bind_rows(
 ) %>%
   mutate(id = factor(id, c("A", "a")))
 
-fallin <- tibble(
-  id = factor(rep("a", 2), c("A", "a")),
-  x = c(7, 27),
-  y = -0.5,
-  label = "\U2714"
-)
-
 ggplot() +
-  geom_line(data = dt, aes(x = date, y = val, group = id), color = "grey60") +
-  geom_vline(data = circle, aes(xintercept = x), linetype = "longdash", color = "grey60") +
-  geom_errorbar(data = errorbar, aes(xmin = x, xmax = xend, y = -0.5), width = 0.5) +
-  geom_point(data = circle, aes(x = x, y = y), color = "brown") +
-  geom_text(data = fallin, aes(x = x, y = y, label = label), color = "brown", size = 5) +
+  geom_rect(data = errorbar, aes(xmin = x, xmax = xend, ymin = 0, ymax = 10), fill = "grey90") +
+  geom_line(data = dt, aes(x = date, y = val, group = id), color = "black") +
+  geom_vline(data = circle %>% filter(id == "A"), 
+             aes(xintercept = x), linetype = "longdash", color = "grey10", lwd = 0.2) +
+  geom_point(data = circle, aes(x = x, y = y, color = match), size = 3) +
   facet_wrap(vars(id), nrow = 2) +
   scale_y_continuous(breaks = seq(0, 10, 2)) +
   scale_x_continuous(breaks = seq(1, 31, 1)) +
+  scale_color_brewer(palette = "Dark2") + 
   theme_bw() +
-  theme(panel.grid = element_blank()) +
+  theme(panel.grid = element_blank(),
+        legend.position = "bottom") +
   labs(x = "Time", y = "Value")
 
 ggsave(filename = "figures/illu-matching.png", width = 10, height = 5)
