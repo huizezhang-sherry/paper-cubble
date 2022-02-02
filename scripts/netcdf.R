6e33365d0f4e8712016d1292a85fde356094bcf9
Sherry Zhang <huizezhangsh@gmail.com>
2022-02-02 13:55:11 +1100

    netcdf example with era5 data

--- scripts/netcdf.R
+++ scripts/netcdf.R
@@ -1,27 +1,59 @@
 library(tidyverse)
 library(cubble)
+library(ecmwfr)
 
-nc <- ncdf4::nc_open(here::here("data/adaptor.mars.internal-1638344933.932894-4194-11-a5d35ac3-b36d-4982-afa5-a5dd193a6718.nc"))
-temp <- as_cubble(nc, vars = "d2m")
-vic_map <- ozmaps::abs_ste %>% filter(NAME == "Victoria")
+my_request <- list(
+  product_type = "reanalysis",
+  format = "netcdf",
+  pressure_level = "10",
+  year = "2002",
+  month = c("09", "10"),
+  day = c("04", "22", "26", "30"),
+  time = "00:00",
+  area = c(-5, -180, -90, 180),
+  variable = c("specific_humidity", 'geopotential', 'potential_vorticity', 'temperature'),
+  dataset_short_name = "reanalysis-era5-pressure-levels",
+  target = "era5-pressure.nc"
+)
 
-out <- temp %>% 
-  stretch(ts) %>% 
-  mutate(d2m = d2m - 273.15) %>% 
-  filter(lubridate::hour(time) == 12) %>% 
-  group_by(id) %>% 
-  summarise(d2m = mean(d2m, na.rm= TRUE))
+wf_request(
+  user = "113339",
+  request = my_request,
+  transfer = TRUE,
+  path = "~/Documents/research/paper-cubble/data/")
 
-res <- out %>%
-  migrate(long, lat)
+raw <- ncdf4::nc_open(file.choose())
+dt <- as_cubble(raw, vars = c("q", "z", "pv", "t"))
 
-vic_string <- vic_map %>% sf::st_cast("MULTILINESTRING") 
+date <- c("2002-09-22", "2002-09-26", "2002-09-30", "2002-10-04") %>% as.Date()
+res <- dt %>% 
+  stretch() %>% 
+  filter(lubridate::date(time) %in% date) %>%
+  migrate(long, lat) %>% 
+  mutate(q =  q* 10^6,
+         mq = z + pv / t)
+  
+box = c(xmin = -180, ymin = -90, xmax = 180, ymax = -5)
+sf::sf_use_s2(FALSE)
+country <- rnaturalearth::ne_coastline("small", returnclass = "sf") %>% 
+  sf::st_crop(box) %>% 
+  sf::st_cast("MULTILINESTRING")
 
 res %>% 
   ggplot() +
-  geom_tile(aes(x = long, y = lat, fill = d2m)) +
-  geom_sf(data = vic_string , alpha = 0.5, fill = "transparent") +
+  geom_point(aes(x = long, y = lat, color = q)) +
+  geom_contour(data = res, aes(x = long, y = lat, z = z),
+               color = "grey20", binwidth = 4000, size = 0.5) +
+  geom_sf(data = country , alpha = 0.5, fill = "transparent", color = "lightgreen") +
+  coord_sf(crs = "+proj=stere +lat_0=-90 +lon_0=-180 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ", default_crs = sf::st_crs(4326), label_graticule = "EW") + 
   theme_bw() + 
-  colorspace::scale_fill_continuous_sequential("Mint")
+  theme(legend.position = "bottom",
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(), 
+        panel.grid = element_blank()) + 
+  facet_wrap(vars(as.Date(time)), nrow = 1, shrink = TRUE)+  
+  colorspace::scale_color_continuous_sequential("Purple-Yellow", name = "Specific humidity") + 
+  labs(x ="", y = "") 
 
-
+ggsave(file = "figures/netcdf.png", width = 15, height = 5)
