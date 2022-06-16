f0361b720d43583a5acbc27463442c27dbbaa9dc
Sherry Zhang <huizezhangsh@gmail.com>
2022-06-16 22:20:26 +1000

    use magritte pipe throughout

--- scripts/interactive-linking.R
+++ scripts/interactive-linking.R
@@ -3,29 +3,29 @@ library(tidyverse)
 library(crosstalk)
 library(plotly)
 
-clean <- weatherdata::climate_full |>
+clean <- weatherdata::climate_full %>%
   filter(!id %in% c("ASN00067033", "ASN00072091", "ASN00059040", 
-                    "ASN00097053", "ASN00040856", "ASN00015548")) |>
-  face_temporal() |> 
-  mutate(month = lubridate::month(date)) |>
-  group_by(month) |>
+                    "ASN00097053", "ASN00040856", "ASN00015548")) %>%
+  face_temporal() %>% 
+  mutate(month = lubridate::month(date)) %>%
+  group_by(month) %>%
   summarise(tmax = mean(tmax, na.rm = TRUE),
             tmin = mean(tmin, na.rm = TRUE),
-            diff = mean(tmax - tmin, na.rm = TRUE)) |> 
-  ungroup(month) |>
-  mutate(dummy_date = as.Date(glue::glue("2021-{month}-01"))) |> 
-  face_spatial() |> 
-  mutate(temp_diff_var = ts |> pull(diff) |> var(na.rm = TRUE))
+            diff = mean(tmax - tmin, na.rm = TRUE)) %>% 
+  ungroup(month) %>%
+  mutate(dummy_date = as.Date(glue::glue("2021-{month}-01"))) %>% 
+  face_spatial() %>% 
+  mutate(temp_diff_var = ts %>% pull(diff) %>% var(na.rm = TRUE))
   
 
-nested <- clean |> 
+nested <- clean %>% 
   SharedData$new(~id, group = "cubble")
 
-long <- clean |> face_temporal() |> unfold(temp_diff_var) |> arrange(temp_diff_var) |> 
+long <- clean %>% face_temporal() %>% unfold(temp_diff_var) %>% arrange(temp_diff_var) %>% 
   SharedData$new(~id, group = "cubble")
 
 state_map <- rmapshaper::ms_simplify(ozmaps::abs_ste, keep = 2e-3)
-p1 <- nested |> 
+p1 <- nested %>% 
   ggplot() +
   geom_sf(data = state_map, aes(geometry = geometry), fill = "transparent", color = "grey50") +
   geom_point(aes(x = long, y = lat,color = temp_diff_var, label = name)) +
@@ -38,7 +38,7 @@ p1 <- nested |>
   theme(panel.grid.major = element_blank(),
         legend.position = "none")
 
-p2 <- long |> 
+p2 <- long %>% 
   ggplot(aes(x = dummy_date, group = id, fill = temp_diff_var, color = temp_diff_var)) +
   geom_ribbon(aes(ymin = tmin, ymax = tmax), size = 0.1, alpha = 0.3) + 
   geom_line(aes(y = tmin), alpha = 0.1) + 
@@ -58,9 +58,9 @@ p2 <- long |>
         legend.position = "bottom")
 
 out <- bscols(
-  ggplotly(p1, width = 900, height = 700, tooltip = "label") |>
+  ggplotly(p1, width = 900, height = 700, tooltip = "label") %>%
     highlight(on = "plotly_selected", off = "plotly_deselect", opacityDim = 0.1),
-  ggplotly(p2, width = 1200, height = 700, tooltip = "label") |> 
+  ggplotly(p2, width = 1200, height = 700, tooltip = "label") %>% 
     highlight(on = "plotly_selected", off = "plotly_deselect", opacityDim = 0.012),
   widths = c(5, 5)
 )
